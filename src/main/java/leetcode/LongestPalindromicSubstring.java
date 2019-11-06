3096251e7dee6a09bd1ec53e3c2a018860b37949
liuyang <liuyang04@kanzhun.com>
2019-11-06 17:07:25 +0800

    内嵌jetty

---
+++ src/main/java/leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * @description:
+ * @author: Liuyang
+ * @date: 2019-10-18 15:23
+ **/
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @description: Two Sum
@@ -11,12 +13,13 @@ public class TwoSum {
 
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        System.out.println(Arrays.toString(twoSum(nums, target)));
+        int target = 26;
+        System.out.println(Arrays.toString(otherSolution(nums, target)));
     }
 
     /**
      * 时间复杂度O(n^2),空间复杂度O(1)
+     *
      * @param nums
      * @param target
      * @return
@@ -35,16 +38,28 @@ public class TwoSum {
     }
 
     /**
-     *
+     * 时间复杂度O(n),空间复杂度O(n)
      * @param nums
      * @param target
      * @return
      */
-    private static int[] otherSolution(int[] nums, int target){
+    private static int[] otherSolution(int[] nums, int target) {
         int[] result = new int[2];
-
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            if (map.containsKey(target - x)) {
+                result[0] = map.get(target - x);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
 
 
         return result;
     }
+
+
+
 }
